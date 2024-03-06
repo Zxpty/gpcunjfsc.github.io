c50120c887230d1393912e599bb95f11cb709970
Jhon Oblitas <134904618+Zxpty@users.noreply.github.com>
2024-03-05 22:35:45 -0500

    feat[resources]: create a page for resources

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,18 +1,23 @@
 export default function Blog() {
     return (
-        <main>
-            <section className="w-full  mx-auto py-32 items-center flex flex-col justify-center">
-                <div className="section py-16 md:py-36 scroll-m-20 w-full mx-auto container lg:max-w-4xl md:max-w-2xl">
-                    <div className="max-w-xl" >
-                        <h2 className="text-xl  opacity-60 pb-8 text-center max-md:w-full max-md:text-lg" >
-                            El Grupo de Estudio de Programación Competitiva de la Universidad José Faustino Sánchez Carrión (GPC-UNJFSC)
-                            es una comunidad dinámica compuesta por estudiantes apasionados por la programación y el desarrollo de algoritmos.
-                            Nos esforzamos por promover el aprendizaje y la práctica del Análisis y Diseño de Algoritmos, con el firme propósito
-                            de preparar a nuestros miembros para competir en torneos de programación a nivel local, nacional e internacional.
-                        </h2>
-                    </div>
+        <div className="section py-28 md:py-28 scroll-m-20 w-full mx-auto container lg:max-w-4xl md:max-w-2xl">
+            <div className="max-w-4xl gap-6 flex flex-col">
+                <div>
+                    <h1 className="text-4xl font-bold  pb-2 max-md:w-full  max-md:text-3xl text-white">Nosotros</h1>
+                    <p className="text-xl opacity-70  pb-4 max-md:w-full  max-md:text-lg [&>strong]:text-blue-500 " >
+                        El Grupo de Estudio de Programación Competitiva de la Universidad José Faustino Sánchez
+                        Carrión, es una comunidad dinámica compuesta por estudiantes apasionados
+                        por la programación y el desarrollo de algoritmos. Nos esforzamos por promover el aprendizaje y la práctica
+                        del Análisis y Diseño de Algoritmos, con el firme propósito de preparar a nuestros miembros para competir en
+                        torneos de programación a nivel local, nacional e internacional.
+                    </p>
+                    <p className="text-pretty">
+                        Nos enorgullece ser una comunidad inclusiva y diversa, donde se valora la creatividad, el esfuerzo y el compañerismo.
+                        Sólo necesitas querer apoyar para poder ser parte de nosotros.
+                        🙌
+                    </p>
                 </div>
-            </section>
-        </main>
+            </div>
+        </div>
     )
 }
--- src/Components/Preparate.jsx
+++ src/Components/Preparate.jsx
@@ -1,20 +1,44 @@
-export default function Preparate(){
-    return(
-        <main>
-        <section className="w-full  mx-auto py-32 items-center flex flex-col justify-center">
-            <h1 className="text-white text-5xl font-bold flex flex-row gap-x-4 pb-2 text-center max-md:text-3xl">
-                Programación Competitiva UNFJSC
-            </h1>
-            <h2 className="text-xl opacity-60 pb-8 text-center max-md:w-full w-3/5 max-md:text-lg" >
-                Bienvenidos, les presentaremos información sobre el grupo de estudio y la Programacion Competitiva.
-                En el cual compartiremos muchas experiencias y competencias en las cuales puedes participar.
-            </h2>
-        </section>
-        <section className="flex flex-col  w-full items-center mx-auto py-2">
-            <div>
-            <div className="flex text-center text-3xl font-bold pb-4">Ultimas Noticias</div>
+export default function Preparate() {
+    return (
+        <div className="section py-28 md:py-28 scroll-m-20 w-full mx-auto container lg:max-w-4xl md:max-w-2xl">
+            <div className="max-w-4xl gap-6 flex flex-col">
+                <div>
+                    <h1 className="text-4xl font-bold  pb-2 max-md:w-full  max-md:text-3xl text-white">Recursos</h1>
+                    <p className="text-xl opacity-70  pb-4 max-md:w-full  max-md:text-lg [&>strong]:text-blue-500 " >
+                        Esta sección es donde podras encontrar material para poder entrenar de manera autodidacta.
+                    </p>
+                    <p className="text-pretty">
+                        Podras encontrar una gran diversidad de recursos, libros, paginas web, canales de Youtube,
+                        páginas para competir, entre eso y mucho más, te desamos lo mejor de los exitos⭐
+                    </p>
+                    <p className="text-pretty pt-2 [&>strong]:text-blue-500">
+                        <strong>Importante: </strong>Se recomienda usar el lenguaje de programación c++ por la efiencia y rapidez
+                        con la que cuenta en cuanto a la entrada y salida de datos.
+                    </p>
+                </div>
+                <div>
+                    <h1 className="text-3xl font-bold  pb-2 pt-2 max-md:w-full  max-md:text-3xl text-white">Guías de Aprendizaje</h1>
+                    <p className="opacity-70  pb-4  max-md:w-full  max-md:text-lg [&>strong]:text-blue-500 " >
+                        Esta guia estara basada con recursos en los cuales hemos podido investigar y recolectar de diferentes páginas
+                        la cuales de una u otra manera les servira de mucho apoyo si recien estan empezando.
+                    </p>
+                    <ul className="list-disc pl-5 ">
+                        <li ><a className="hover:border-b-2 hover:border-blue-500" href="">Introducción C++ - OmegaUp</a></li>
+                        <li ><a className="hover:border-b-2 hover:border-blue-500" href="">Learn Cpp and Data Structures - CodeChef</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1 className="text-3xl font-bold  pb-2 pt-2 max-md:w-full  max-md:text-3xl text-white">Páginas con Documentación</h1>
+                    <p className="opacity-70  pb-4  max-md:w-full  max-md:text-lg [&>strong]:text-blue-500 " >
+                        Estas páginas sirven de mucho, ya que puedes buscar información detallada de las estructuras por si tienes dudas o como implementar.
+                    </p>
+                    <ul className="list-disc pl-5 ">
+                        <li ><a className="hover:border-b-2 hover:border-blue-500" href="https://cp-algorithms.com/">CP Algorithms</a></li>
+                        <li ><a className="hover:border-b-2 hover:border-blue-500" href="http://wcipeg.com/wiki/Special:AllPages">P3GWiki</a></li>
+                        <li ><a className="hover:border-b-2 hover:border-blue-500" href="https://en.cppreference.com/w/">Cpp Reference</a></li>
+                    </ul>
+                </div>
             </div>
-        </section>
-    </main>
+        </div>
     )
 }
