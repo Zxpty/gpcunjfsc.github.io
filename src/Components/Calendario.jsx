9b6d4fad7d8bb1e56e72ce0db0043806e47d6c2d
Anthony <Anthony-08.2004@hotmail.com>
2024-03-23 00:54:35 -0500

    test

--- src/Components/Calendario.jsx
+++ src/Components/Calendario.jsx
@@ -3,7 +3,7 @@ export default function Calendar() {
         <div className="section py-28 md:py-28 scroll-m-20 w-full mx-auto container lg:max-w-4xl md:max-w-2xl">
             <div className="max-w-4xl gap-6 flex flex-col">
                 <div>
-                    <h1 className="text-4xl font-bold text-center  pb-2 max-md:w-full  max-md:text-3xl text-white">Muy Pronto...</h1>
+                    <h1 className="text-4xl font-bold text-center  pb-2 max-md:w-full  max-md:text-3xl text-white">Muy Pronto Nuevas Cosas...</h1>
                 </div>
             </div>
         </div>
