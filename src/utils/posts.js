1cf9288215d84b63bc3a94899da254e2a2e8a654
Anthonyx <Anthony-08.2004@hotmail.com>
2024-05-15 21:16:36 -0500

    add new news

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -1,33 +1,31 @@
-import BienCachi from '../assets/images/news/Bienvenida.png'
-import Sergio from '../assets/images/news/Sergio.png'
-import Rafael from '../assets/images/news/Rafael.png'
+import LlamaCode from '../assets/images/news/LlamaCode2024.jpeg'
 export const posts = [
     {
         id: 1,
-        pic: BienCachi,
-        title: "Bienvenida de Cachimbos 2024",
-        shortDesc: "Este 2 de abril, se estara dando inicio a la bienvenida de los nuevos ingresantes a la carrera de Ingeniería de Sistemas.",
+        pic: LlamaCode,
+        title: "LlamaCode-2024",
+        shortDesc: "Únete a nuestro concurso de programación y demuestra tus habilidades resolviendo desafiantes problemas de algoritmia. Esta es una excelente oportunidad para poner a prueba tus conocimientos, aprender nuevas técnicas y competir por increíbles premios.",
 
     },
     {
         id: 2,
-        pic: Sergio,
-        title: "Rust: El lenguaje de bajo nivel más querido por 8 años consecutivos.",
-        shortDesc: "Aqui hablaremos sobre Rust, su impacto, el porque se convirtio en el lenguaje mas querido, que beneficios tiene y porque las empresas lo estan adoptando",
+        pic: LlamaCode,
+        title: "LlamaCode-2024",
+        shortDesc: "Únete a nuestro concurso de programación y demuestra tus habilidades resolviendo desafiantes problemas de algoritmia. Esta es una excelente oportunidad para poner a prueba tus conocimientos, aprender nuevas técnicas y competir por increíbles premios.",
 
     },
     {
         id: 3,
-        pic: Rafael,
-        title: "Gestión de proyectos con SCRUM",
-        shortDesc: "¡Prepárate para sumergirte en el emocionante mundo de la gestión de proyectos con SCRUM de la mano de Rafael Huapaya!",
+        pic: LlamaCode,
+        title: "LlamaCode-2024",
+        shortDesc: "Únete a nuestro concurso de programación y demuestra tus habilidades resolviendo desafiantes problemas de algoritmia. Esta es una excelente oportunidad para poner a prueba tus conocimientos, aprender nuevas técnicas y competir por increíbles premios.",
 
     },
     {
         id: 4,
-        pic: BienCachi,
-        title: "Coming Soon",
-        shortDesc: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        pic: LlamaCode,
+        title: "LlamaCode-2024",
+        shortDesc: "Únete a nuestro concurso de programación y demuestra tus habilidades resolviendo desafiantes problemas de algoritmia. Esta es una excelente oportunidad para poner a prueba tus conocimientos, aprender nuevas técnicas y competir por increíbles premios."
 
     }
 ]
